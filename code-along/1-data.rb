72729befa98aa495abc54d98513f965dbb52b738
dmostowfi <deanna.mostowfi@kellogg.northwestern.edu>
2022-04-22 18:49:01 +0000

    start

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -1,16 +1,24 @@
 # To run this code, be sure your current working directory
 # is the same as where this file is located and then run:
 # ruby 1-data.rb
+puts"Hello World" # = "put" a "s"tring to the screen
 
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 7
 
 # Perform simple math with numbers
+puts 5+3
 
 # Strings
+puts "tacos"
+#ruby is very forgiving, so much so that you can combine strings and integers! 
+puts "tacos"*3
+#but this weirdly doesn't work  #puts "tacos"+3
 
 # Combine strings together
+puts "#{3+2} tacos" #the #{} allows you to put in calculations, but still turn it into a string
 
 # Variables
 
