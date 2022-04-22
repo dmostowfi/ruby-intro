1b8f184895e946204b88d46b430ee6e53181c04d
dmostowfi <deanna.mostowfi@kellogg.northwestern.edu>
2022-04-22 20:32:54 +0000

    along hashes

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,29 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+#think of this as representing Deanna's social media profile
+me = {
+    "name" => "Deanna",
+    "location" => "NYC", 
+    "username" => "deanna_mo"
+}
+puts me
 
 # Accessing data from the hash
 
+puts me["name"] #as opposed to an array, where you need to specific the location [0]
+
+#redefining attribute "name"
+me["name"] = {"first_name" => "Deanna", "last_name" => "Mostowfi"}
+puts me["name"]["last_name"]
+
 # More Complex Hashes
+
+me = {
+    "name" => "Deanna",
+    "location" => {"city" => "New York", "state" => "NY"}, 
+    "username" => "deanna_mo"
+}
+puts me["location"]
+puts me["location"]["city"]
+
